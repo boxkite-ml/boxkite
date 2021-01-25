27f65a2860238f2a19b23eee1a8468088733d9f5
Luke Marsden <me@lukemarsden.net>
2021-01-25 14:09:23 +0000

    build & publish setup

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# https://packaging.python.org/tutorials/packaging-projects/
+
+python3 setup.py sdist bdist_wheel
---
+++ publish.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# See https://packaging.python.org/tutorials/packaging-projects/
+
+python3 -m twine upload dist/*
