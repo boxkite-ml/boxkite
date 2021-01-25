4f1b521cb8df1242de1cbd0f8d33d7e9d971dfd5
Luke Marsden <me@lukemarsden.net>
2021-01-25 14:05:31 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="boxkite-lukemarsden",
+    version="0.0.1",
+    author="Luke Marsden",
+    author_email="me@lukemarsden.net",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/basisai/boxkite",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
