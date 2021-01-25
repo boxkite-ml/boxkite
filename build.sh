1491af2803c0022f22a4e97736a20fc0b3ad4d9f
Luke Marsden <me@lukemarsden.net>
2021-01-25 14:20:30 +0000

    clear out dist directory before uploading

--- build.sh
+++ build.sh
@@ -2,4 +2,10 @@
 
 # https://packaging.python.org/tutorials/packaging-projects/
 
+echo "Danger Will Robinson! Will delete your dist directory in 10 seconds"
+echo "Press ^C to cancel."
+sleep 10
+
+rm -rf dist
+
 python3 setup.py sdist bdist_wheel
