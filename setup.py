b0fda12cfdbbd65e3bad3a112c70213de7ce1ae0
Luke Marsden <me@lukemarsden.net>
2021-03-04 13:10:38 +0000

    bump boxkite to 0.0.2 with transitive dependency fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="boxkite",
-    version="0.0.1",
+    version="0.0.2",
     author="Luke Marsden",
     author_email="me@lukemarsden.net",
     description="A small example package",
