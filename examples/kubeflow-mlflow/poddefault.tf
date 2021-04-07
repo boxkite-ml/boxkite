087061957e26ab2892af00f3df02c833c1b3efe8
Luke Marsden <me@lukemarsden.net>
2021-04-07 13:09:43 +0100

    what to do next

--- examples/kubeflow-mlflow/poddefault.tf
+++ examples/kubeflow-mlflow/poddefault.tf
@@ -1,7 +1,17 @@
-resource "k8s_manifest" "mlflow_poddefault" {
-  depends_on = [kubernetes_deployment.kubeflow_operator, k8s_manifest.kubeflow_application_crd]
+/*resource "k8s_manifest" "mlflow_poddefault" {
+  depends_on = [module.kubeflow.wait_for_kubeflow, kubernetes_namespace.admin]
   timeouts {
     delete = "15m"
   }
-  content = templatefile("${path.module}/poddefaults.yaml", {})
+  content = templatefile("${path.module}/poddefault.yaml", {})
 }
+
+resource "kubernetes_namespace" "admin" {
+  metadata {
+    name = "admin"
+  }
+}*/
+
+// TODO: try and extract the profile CRD from kubeflow and pre-create it in
+// terraform so that the setup wizard doesn't fail with the namespace already
+// existing.
---
+++ examples/kubeflow-mlflow/terraform-module-kubeflow/outputs.tf
@@ -0,0 +1,3 @@
+output "wait_for_kubeflow" {
+  value = k8s_manifest.kubeflow_kfdef
+}
