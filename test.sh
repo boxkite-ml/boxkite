19c2e71c149767c54757f0aca875ffeae39081d0
Han Qiao <qiao@basis-ai.com>
2021-04-08 16:23:56 +0800

    Integrate with codecov

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 isort --profile black --diff --check-only boxkite tests
 black --check boxkite tests
 
-pytest -svl --log-level=DEBUG --color=yes --cov=boxkite --cov-fail-under=90 --cov-report=html --cov-report=xml tests
+pytest -svl --log-level=DEBUG --color=yes --cov=boxkite --cov-fail-under=90 --cov-report=xml tests
