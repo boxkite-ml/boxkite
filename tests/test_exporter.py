3e7e8727c7cc6f3be2892e2d3277710ebe2b7ceb
Han Qiao <qiao@basis-ai.com>
2021-04-09 16:48:50 +0800

    Add tests for fluentd exporter

---
+++ tests/test_exporter.py
@@ -0,0 +1,20 @@
+from os import environ
+
+from boxkite.monitoring.exporter.fluentd_exporter import BEDROCK_FLUENTD_ADDR
+from boxkite.monitoring.service import ModelMonitoringService
+from tests.mockserver import MockRecvServer
+
+
+def test_fluent_sender():
+    server = MockRecvServer(port=24224)
+    environ[BEDROCK_FLUENTD_ADDR] = "localhost"
+    service = ModelMonitoringService()
+
+    pid = service.log_prediction(
+        request_body="test", features=[2.0, 1.2, 0.8], output="dog"
+    )
+    service._log_exporter._sender.close()
+
+    item = server.get_received()[0]
+    assert item[1] == item[2][b"created_at"]
+    server.close()
