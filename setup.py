c708548863b259a794e31c57d6011bd75b62f216
Han Qiao <qiao@basis-ai.com>
2021-04-14 15:43:07 +0800

    Release boxkite 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="boxkite",
-    version="0.0.3",
+    version="0.0.4",
     author="Luke Marsden",
     author_email="me@lukemarsden.net",
     description="A small example package",
